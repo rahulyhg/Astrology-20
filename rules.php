cc8cc155e87e5bc9994a822725d232f2b78754b1
ismailcaakir <ismailcakirnet@gmail.com>
2018-09-17 14:23:27 +0300

    kod temizlendi

--- rules.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Available placeholders: ismailcaakir, Astrology, ismailcaakir, astrology
-return [
-    'src/MyPackage.php' => 'src/Astrology.php',
-    'config/mypackage.php' => 'config/astrology.php',
-    'src/Facades/MyPackage.php' => 'src/Facades/Astrology.php',
-    'src/MyPackageServiceProvider.php' => 'src/AstrologyServiceProvider.php',
-];
